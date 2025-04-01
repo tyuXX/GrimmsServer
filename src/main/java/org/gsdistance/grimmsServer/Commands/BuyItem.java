6b9c75353c7fee187282a07dbf07a1279ac9bdf9
tyuXX <tyuxx@gsdistance.org>
2025-04-01 22:58:38 +0300

    Fix

--- src/main/java/org/gsdistance/grimmsServer/Commands/BuyItem.java
+++ src/main/java/org/gsdistance/grimmsServer/Commands/BuyItem.java
@@ -15,16 +15,16 @@ public class BuyItem implements CommandExecutor {
                 sender.sendMessage("Usage: /buy <item> <amount>");
                 return false;
             }
-            if(Material.matchMaterial(args[0].toLowerCase()) == null){
+            if(Material.matchMaterial(args[0]) == null){
                 sender.sendMessage("Invalid item id:" + args[0]);
                 return false;
             }
             Market market = Market.getMarket();
-            if(market.items.get(args[0]) >= Integer.parseInt(args[1])){
+            if(market.items.get(Material.matchMaterial(args[0]).getKey().toString()) <= Integer.parseInt(args[1])){
                 sender.sendMessage("Not enough stock of " + args[0] + " in the market.");
                 return false;
             }
-            double bought = market.buy(Material.matchMaterial(args[0].toLowerCase()), Integer.parseInt(args[1]), (Player) sender);
+            double bought = market.buy(Material.matchMaterial(args[0]), Integer.parseInt(args[1]), (Player) sender);
             market.saveMarket();
             sender.sendMessage("You bought " + args[1] + " many of " + args[0] + " for " + Math.max(0.25D,Math.round((bought))));
             return true;
