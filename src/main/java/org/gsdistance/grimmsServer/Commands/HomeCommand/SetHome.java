56b12224a67e5e72de81a29ee39b79fcfc7f6568
tyuXX <tyuxx@gsdistance.org>
2025-06-23 14:28:24 +0300

    Cleanup

--- src/main/java/org/gsdistance/grimmsServer/Commands/HomeCommand/SetHome.java
+++ src/main/java/org/gsdistance/grimmsServer/Commands/HomeCommand/SetHome.java
@@ -5,6 +5,7 @@ import org.gsdistance.grimmsServer.Constructable.Location;
 import org.gsdistance.grimmsServer.Constructable.PlayerMetadata;
 
 public class Sethome {
+    @SuppressWarnings("SameReturnValue")
     public static boolean SubCommand(Player player, String[] args) {
         String homeName = "home";
         if (args.length > 1) {
