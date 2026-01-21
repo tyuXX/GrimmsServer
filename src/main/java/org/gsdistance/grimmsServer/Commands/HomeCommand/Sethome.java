10c011ede1acac7641b173c4293c51ad2a1e08e5
tyuXX <tyuxx@gsdistance.org>
2026-01-22 00:43:46 +0300

    Item market value indexing added

---
+++ src/main/java/org/gsdistance/grimmsServer/Commands/HomeCommand/Sethome.java
@@ -0,0 +1,31 @@
+package org.gsdistance.grimmsServer.Commands.HomeCommand;
+
+import org.bukkit.entity.Player;
+import org.gsdistance.grimmsServer.Constructable.Location;
+import org.gsdistance.grimmsServer.Constructable.Player.PlayerMetadata;
+
+public class Sethome {
+    @SuppressWarnings("SameReturnValue")
+    public static boolean SubCommand(Player player, String[] args) {
+        String homeName = "home";
+        if (args.length > 1) {
+            homeName = args[1].toLowerCase();
+        }
+        PlayerMetadata meta = PlayerMetadata.getPlayerMetadata(player);
+
+        // Check multiHome permission
+        boolean hasMultiHome = player.hasPermission("grimmsserver.multiHome");
+        if (!hasMultiHome) {
+            // Only allow one home (named "home" or the first set)
+            if (!meta.homes.isEmpty() && !meta.homes.containsKey(homeName)) {
+                player.sendMessage("You do not have permission to set multiple homes.");
+                return true;
+            }
+        }
+
+        meta.homes.put(homeName, new Location(player.getLocation()));
+        meta.saveToPDS();
+        player.sendMessage("Home '" + homeName + "' set!");
+        return true;
+    }
+}
